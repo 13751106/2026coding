7dcea5587c3a8ac650a1fcaa5c30342364e03719
13751106 <13751106@me.mcu.edu.tw>
2026-05-07 16:22:43 +0800

    week11

---
+++ week11/week11-1a.py
@@ -0,0 +1,16 @@
+# week11-1a.py
+# LeetCode 872. Leaf-Similar Trees
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        a = []
+        def helper(root):
+            if root.left == None and root.right == None:
+                a.append( root.val )
+            if root.left: helper(root.left)
+            if root.right: helper(root.right)
+        helper(root1)
+        a, b = [], a
+        helper(root2)
+        #print('a', a)
+        #print('b', b)
+        return a == b
